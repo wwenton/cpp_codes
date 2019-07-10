17923c99f23b232ebec4476c21e5e9198401f0a1
Nurbek Erkulov <wwenton@protonmail.com>
2019-07-10 19:34:26 +0300

    mm

--- debug.cpp
+++ debug.cpp
@@ -1,34 +1,40 @@
 
 template <class c> auto try_out(c x) -> decltype(cerr << x, 0);
 char try_out(...);
-
 template <class c> struct range { c b, e; };
 template <class c> range<c> mrange(c i, c j) { return range<c>{i, j}; }
 
 struct err {
 
-	template <class c, class d> err& operator << (pair<c,d> x) {
-		return *this << "(" << x.first << ", " << x.second << ")";
-	}
-	
-	template <class c> typename enable_if<sizeof(try_out(declval<c>())) != 1, err&>::type operator << (c x) {
-		cerr << boolalpha << x;
-		return *this;
-	}
-	
-	template <class c> typename enable_if<sizeof(try_out(declval<c>())) == 1, err&>::type operator << (c x) {
-		return *this << mrange(begin(x), end(x));
-	}
+  template <class c, class d> err& operator << (pair<c,d> x) {
+    return *this << "(" << x.first << ", " << x.second << ")";
+  }
 
-	template <class c> err& operator << (range<c> x) {
-		*this << "{";
-		for (auto it = x.b; it != x.e; it++)
-			*this << ", " + 2 * (it == x.b) << *it;
-		return *this << "}";
-	}
+  template <class c> typename enable_if<sizeof(try_out(declval<c>())) != 1, err&>::type operator << (c x) {
+    cerr << boolalpha << x;
+    return *this;
+  }
+
+  template <class c> typename enable_if<sizeof(try_out(declval<c>())) == 1, err&>::type operator << (c x) {
+    return *this << mrange(begin(x), end(x));
+  }
+
+  template <class c> err& operator << (range<c> x) {
+    *this << "{";
+    for (auto it = x.b; it != x.e; it++)
+      *this << ", " + 2 * (it == x.b) << *it;
+    return *this << "}";
+  }
 
 };
 
 #define aty(...) "[ " << #__VA_ARGS__ << " : " << __VA_ARGS__ << " ]"
+
+#ifdef MENIN_COMPUM
+  #define debug(x) err() << aty(x) << "\n"
+#else
+  #define debug(x) 42
+#endif
+
 
 
---
+++ template.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+#define sz(x) ((int) (x).size())
+#define all(x) (x).begin(), (x).end()
+#define forn(i, n) for(int i = 0; i < n; ++i)
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+
+
+
+int32_t main() {
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+	
+	return 0;
+}
